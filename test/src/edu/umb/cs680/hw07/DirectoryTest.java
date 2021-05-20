a114392aec0a17b3c7802ecb4841faa9bb4017a4
Annie Moriondo <anniemoriondo@gmail.com>
2021-05-20 02:12:48 -0400

    finished setting up fixture instance variables

--- test/src/edu/umb/cs680/hw07/DirectoryTest.java
+++ test/src/edu/umb/cs680/hw07/DirectoryTest.java
@@ -6,7 +6,13 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.AfterAll;
 
 public class DirectoryTest {
+    // Instance variables from our test fixture.
     private static FileSystem fs;
+    private static Directory root;
+    private static Directory home;
+    private static Directory applications;
+    private static Directory code;
+    private static Directory pics;
 
     private String[] dirToStringArray(Directory d){
         String parentName = d.getParent() == null ?
@@ -26,49 +32,52 @@ public class DirectoryTest {
     @BeforeAll
     public static void setUpFS(){
         fs = TestFixtureInitializer.createFS();
+        root = fs.getRootDirs().get(0);
+        applications = (Directory) root.getChildren().get(0);
+        home = (Directory) root.getChildren().get(1);
+        code = (Directory) home.getChildren().get(0);
+        pics = (Directory) home.getChildren().get(1);
     }
 
     @Test
     public void verifyDirectoryEqualityRoot(){
         String[] expected = {"true", "root", "0", "null", "2"};
-        Directory actual = fs.getRootDirs().getFirst();
-        assertArrayEquals(expected, dirToStringArray(actual));
+        assertArrayEquals(expected, dirToStringArray(root));
 
     }
 
     @Test
     public void verifyDirectoryEqualityApplications(){
         String[] expected = {"true", "applications", "0", "root", "1"};
-        // Get the `applications` directory.
-        Directory actual = (Directory)
-                fs.getRootDirs().getFirst().getChildren().getFirst();
-        assertArrayEquals(expected, dirToStringArray(actual));
+        assertArrayEquals(expected, dirToStringArray(applications));
     }
 
     @Test
     public void verifyDirectoryEqualityHome(){
         String[] expected = {"true", "home", "0", "root", "3"};
-        // Get the `home` directory.
-        Directory actual = (Directory)
-                fs.getRootDirs().getFirst().getChildren().getLast();
-        assertArrayEquals(expected, dirToStringArray(actual));
+        assertArrayEquals(expected, dirToStringArray(home));
     }
 
     @Test
     public void verifyDirectoryEqualityCode(){
         String[] expected = {"true", "code", "0", "home", "2"};
-        // Get the `code` directory.
-        Directory parent = (Directory) fs.getRootDirs().getFirst().getChildren().getLast();
-        Directory actual = (Directory) parent.getChildren().getFirst();
-        assertArrayEquals(expected, dirToStringArray(actual));
+        assertArrayEquals(expected, dirToStringArray(code));
     }
 
     @Test
     public void verifyDirectoryEqualityPics(){
         String[] expected = {"true", "pics", "0", "home", "2"};
-        // Get the `pics` directory.
-        Directory parent = (Directory) fs.getRootDirs().getFirst().getChildren().getLast();
-        Directory actual = (Directory) parent.getChildren().get(1);
-        assertArrayEquals(expected, dirToStringArray(actual));
+        assertArrayEquals(expected, dirToStringArray(pics));
     }
+
+    @Test
+    public void verifyCountChildrenRoot(){}
+
+    public void verifyCountChildrenApplications(){}
+
+    public void verifyCountChildrenHome(){}
+
+    public void verifyCountChildrenCode(){}
+
+    public void verifyCountChildrenPics(){}
 }
